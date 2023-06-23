dc7e21aa21ae888f8896c3b0dde0aab4d962f1b3
Tabinka <tabicode@protonmail.com>
2023-06-23 23:14:14 +0200

    Create Simple Main Menu
    Simple main menu with basic functionality and design. Buttons Play and Quit are prepared.

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        // Load the first level. Make sure your game scene is added in the Build Settings to be loaded.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
